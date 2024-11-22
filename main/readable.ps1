4b215b60c0a0ff5b1e1c825423d8d6f2f851bfa6
Jeff Hykin <jeff.hykin@gmail.com>
2024-11-21 19:46:15 -0600

    add current deno installer

--- main/readable.ps1
+++ main/readable.ps1
@@ -21,71 +21,104 @@
     has () {
         command -v "$1" >/dev/null;
     } ; 
-    # try to install unzip for the user if its missing
-    if ! has unzip; then
-        if ! has apt-get; then 
-            has brew && brew install unzip;
-        else 
-            if [ "$(whoami)" = "root" ]; then 
-                apt-get install unzip -y;
-            elif has sudo; then 
-                echo "Can I install unzip for you? (its required for this command to work) ";read ANSWER;echo; 
-                if [ "$ANSWER" =~ ^[Yy] ]; then 
-                    sudo apt-get install unzip -y;
-                fi;
-            elif has doas; then 
-                echo "Can I install unzip for you? (its required for this command to work) ";read ANSWER;echo; 
-                if [ "$ANSWER" =~ ^[Yy] ]; then 
-                    doas apt-get install unzip -y;
-                fi;
-            fi; 
-        fi; 
-    fi; 
-    # if still doesn't have unzip
-    if ! has unzip; then 
-        echo "";
-        echo "So I couldn't find an 'unzip' command";
-        echo "And I tried to auto install it, but it seems that failed";
-        echo "(This script needs unzip and either curl or wget)";
-        echo "Please install the unzip command manually then re-run this script";
-        exit 1; 
-    fi; 
-    repo="denoland/deno";
+    set -e
+
+    if ! command -v unzip >/dev/null && ! command -v 7z >/dev/null; then
+        echo "Error: either unzip or 7z is required to install Deno (see: https://github.com/denoland/deno_install#either-unzip-or-7z-is-required )." 1>&2
+        exit 1
+    fi
+
     if [ "$OS" = "Windows_NT" ]; then
-        target="x86_64-pc-windows-msvc";
+        target="x86_64-pc-windows-msvc"
     else
-        # do-nothing command (otherwise one-lining this code breaks)
-        :; 
         case $(uname -sm) in
         "Darwin x86_64") target="x86_64-apple-darwin" ;;
         "Darwin arm64") target="aarch64-apple-darwin" ;;
-        "Linux aarch64")
-            repo="LukeChannings/deno-arm64"
-            target="linux-arm64"
-            ;;
-        "Linux armhf")
-            echo "deno sadly doesn't support 32-bit ARM. Please check your hardware and possibly install a 64-bit operating system."
-            exit 1
-            ;;
+        "Linux aarch64") target="aarch64-unknown-linux-gnu" ;;
         *) target="x86_64-unknown-linux-gnu" ;;
-        esac;
-    fi;
-    deno_uri="https://github.com/$repo/releases/download/v$version/deno-$target.zip";
-    exe="$bin_dir/deno";
+        esac
+    fi
+
+    print_help_and_exit() {
+        echo "Setup script for installing deno
+
+    Options:
+    -y, --yes
+        Skip interactive prompts and accept defaults
+    --no-modify-path
+        Don't add deno to the PATH environment variable
+    -h, --help
+        Print help
+    "
+        echo "Note: Deno was not installed"
+        exit 0
+    }
+
+    # Simple arg parsing - look for help flag, otherwise
+    # ignore args starting with '-' and take the first
+    # positional arg as the deno version to install
+    for arg in "$@"; do
+        case "$arg" in
+        "-h")
+            print_help_and_exit
+            ;;
+        "--help")
+            print_help_and_exit
+            ;;
+        "-"*) ;;
+        *)
+            if [ -z "$deno_version" ]; then
+                deno_version="$arg"
+            fi
+            ;;
+        esac
+    done
+    if [ -z "$deno_version" ]; then
+        deno_version="$(curl -s https://dl.deno.land/release-latest.txt)"
+    fi
+
+    deno_uri="https://dl.deno.land/release/${deno_version}/deno-${target}.zip"
+    deno_install="${DENO_INSTALL:-$HOME/.deno}"
+    bin_dir="$deno_install/bin"
+    exe="$bin_dir/deno"
+
     if [ ! -d "$bin_dir" ]; then
-        mkdir -p "$bin_dir";
-    fi;
-    
-    if ! curl --fail --location --progress-bar --output "$exe.zip" "$deno_uri"; then
-        if ! wget --output-document="$exe.zip" "$deno_uri"; then
-            echo "Howdy! I looked for the 'curl' and for 'wget' commands but I didn't see either of them. Please install one of them, otherwise I have no way to install the missing deno version needed to run this code";
-            exit 1;
-        fi;
-    fi;
-    unzip -d "$bin_dir" -o "$exe.zip";
-    chmod +x "$exe";
-    rm "$exe.zip";
-    exec "$deno" run -q -A "$0" "$@";
+        mkdir -p "$bin_dir"
+    fi
+
+    curl --fail --location --progress-bar --output "$exe.zip" "$deno_uri"
+    if command -v unzip >/dev/null; then
+        unzip -d "$bin_dir" -o "$exe.zip"
+    else
+        7z x -o"$bin_dir" -y "$exe.zip"
+    fi
+    chmod +x "$exe"
+    rm "$exe.zip"
+
+    echo "Deno was installed successfully to $exe"
+
+    run_shell_setup() {
+        $exe run -A --reload jsr:@deno/installer-shell-setup/bundled "$deno_install" "$@"
+    }
+
+    # If stdout is a terminal, see if we can run shell setup script (which includes interactive prompts)
+    if [ -z "$CI" ] && [ -t 1 ] && $exe eval 'const [major, minor] = Deno.version.deno.split("."); if (major < 2 && minor < 42) Deno.exit(1)'; then
+        if [ -t 0 ]; then
+            run_shell_setup "$@"
+        else
+            # This script is probably running piped into sh, so we don't have direct access to stdin.
+            # Instead, explicitly connect /dev/tty to stdin
+            run_shell_setup "$@" </dev/tty
+        fi
+    fi
+    if command -v deno >/dev/null; then
+        echo "Run 'deno --help' to get started"
+    else
+        echo "Run '$exe --help' to get started"
+    fi
+    echo
+    echo "Stuck? Join our Discord https://discord.gg/deno"
+
     # 
     # powershell portion
     # 
